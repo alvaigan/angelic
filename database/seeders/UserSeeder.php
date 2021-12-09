2c58209fc926c1ada0cedb1176daab1750fd2d8d
alvaigan <aalvaigan@gmail.com>
2021-12-09 15:37:12 +0700

    persiapan 2

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -17,23 +17,9 @@ class UserSeeder extends Seeder
     public function run()
     {
         $user1 = new User;
-        $user1->username = "admin1";
-        $user1->password = Hash::make('admin1', ['rounds' => 10]);
+        $user1->username = "Admin Angelic";
+        $user1->password = Hash::make('@ngel!c', ['rounds' => 10]);
         $user1->role = "admin";
         $user1->save();
-
-        $user2 = new User;
-        $user2->username = "admin2";
-        $user2->password = Hash::make('admin2', ['rounds' => 10]);
-        $user2->role = "admin";
-        $user2->save();
-
-
-        $user3 = new User;
-        $user3->username = "admin3";
-        $user3->password = Hash::make('admin3', ['rounds' => 10]);
-        $user3->role = "admin";
-        $user3->save();
-
     }
 }
