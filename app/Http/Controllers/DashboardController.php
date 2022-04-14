474756e220ecd745404396c707af7175250fefcd
aalvaigan <aalvaigan@gmail.com>
2022-04-15 06:22:36 +0700

    update dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -18,11 +18,46 @@ class DashboardController extends Controller
             return redirect()->route('login.page');
         }
 
-        $data['kategori'] = Kategori::all();
-        $data['tag'] = Tag::all();
-        $data['ukuran'] = Ukuran::all();
-        $data['banner'] = Banner::all();
+        $data['kategori'] = Kategori::all()->count();
+        $data['tag'] = Tag::all()->count();
+        $data['ukuran'] = Ukuran::all()->count();
+        $data['banner'] = Banner::all()->count();
         $data['produk'] = Produk::with('kategori', 'stok')->get();
+        $baru = Transaksi::where('status', 'baru')->get();
+        $dibayar = Transaksi::where('status', 'dibayar')->get('status');
+        $dikemas = Transaksi::where('status', 'dikemas')->get('status');
+        $dikirim = Transaksi::where('status', 'dikirim')->get('status');
+        $selesai = Transaksi::where('status', 'selesai')->get('status');
+
+        $json_baru = new \stdClass();
+        $json_baru->status = 'baru';
+        $json_baru->jumlah = $baru->count();
+
+        $json_dibayar = new \stdClass();
+        $json_dibayar->status = 'dibayar';
+        $json_dibayar->jumlah = $dibayar->count();
+
+        $json_dikemas = new \stdClass();
+        $json_dikemas->status = 'dikemas';
+        $json_dikemas->jumlah = $dikemas->count();
+
+        $json_dikirim = new \stdClass();
+        $json_dikirim->status = 'dikirim';
+        $json_dikirim->jumlah = $dikirim->count();
+
+        $json_selesai = new \stdClass();
+        $json_selesai->status = 'selesai';
+        $json_selesai->jumlah = $selesai->count();
+
+        $transaksi = [
+            $json_baru,
+            $json_dibayar,
+            $json_dikemas,
+            $json_dikirim,
+            $json_selesai,
+        ];
+
+        $data['transaksi'] = $transaksi;
         
         return view('dashboard')->with($data);
     }
